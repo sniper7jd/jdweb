f5aca91b1a80640c80951230476b15e6521ce6ba
sniper7jd <dhingra.jagrit15@gmail.com>
2025-12-10 23:43:32 -0500

    Update introduction, experience, modal styling, animation speed, and profile image.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
+        let offset = sec.offsetTop - 250; // Further reduced offset to trigger animation much sooner
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
